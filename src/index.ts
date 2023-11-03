ec2b3a73ffb1b39c220e98096796c4fe69fa97a4
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2023-11-03 19:58:59 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import express from "express";
 // import projects from "./routes/project.routes"
 // import contentGroups from "./routes/contentGroup.routes"
 import contents from "./routes/content.routes"
-
+import cors from "cors"
 import { config } from "./utils/config";
 
-const cors = require('cors')
 const port = config.APP_PORT
 
 const app = express()
-app.use(cors)
+
+// app.use(cors)
 app.use(express.json())
+app.use(cors())
 // app.use('/projects', projects)
 app.use('/contents', contents)
 // app.use('/contentGroups', contentGroups)
